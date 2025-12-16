9a99e640aa1ce9c1758949c5a08bf32685d76e40
Abror <abrordavlatov1103@gmail.com>
2025-12-16 23:51:49 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// Set the date we're counting down to
+const countDownDate = new Date("Dec 21, 2025 14:00:00").getTime();
+
+const timer = setInterval(() => {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = hours;
+  document.getElementById("minutes").innerText = minutes;
+  document.getElementById("seconds").innerText = seconds;
+
+  if (distance < 0) {
+    clearInterval(timer);
+    document.getElementById("timer").innerHTML = "Marosim boshlandi!";
+  }
+}, 1000);
